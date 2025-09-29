f42ec28aff3bb04f8cb930eb97b18a4e475ff7a7
ElephaSolutions <talent.experts@elephasolutions.com>
2025-09-30 00:48:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+1. Updated App.tsx with Authentication
 
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/Login";
 import Index from "./pages/Index";
 import Vehicle from "./pages/Vehicle";
 import VehicleView from "./pages/VehicleView";
@@ -15,45 +19,55 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <div className="min-h-screen">
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route 
-              path="/vehicle" 
-              element={
-                <>
-                  <Navbar />
-                  <Vehicle />
-                </>
-              } 
-            />
-            <Route 
-              path="/vehicle/view" 
-              element={
-                <>
-                  <Navbar />
-                  <VehicleView />
-                </>
-              } 
-            />
-            <Route 
-              path="/license/view" 
-              element={
-                <>
-                  <Navbar />
-                  <LicenseView />
-                </>
-              } 
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </TooltipProvider>
+    <AuthProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <div className="min-h-screen">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route 
+                path="/" 
+                element={
+                  <ProtectedRoute>
+                    <Index />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/vehicle" 
+                element={
+                  <ProtectedRoute>
+                    <Navbar />
+                    <Vehicle />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/vehicle/view" 
+                element={
+                  <ProtectedRoute>
+                    <Navbar />
+                    <VehicleView />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/license/view" 
+                element={
+                  <ProtectedRoute>
+                    <Navbar />
+                    <LicenseView />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </TooltipProvider>
+    </AuthProvider>
   </QueryClientProvider>
 );
 
