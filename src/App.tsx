ef5c42a6174b1baba38238c7b8221ca689868966
ElephaSolutions <talent.experts@elephasolutions.com>
2025-09-30 00:48:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-1. Updated App.tsx with Authentication
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
