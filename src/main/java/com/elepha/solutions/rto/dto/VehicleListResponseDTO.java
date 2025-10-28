1ed58e0376b57530c84d663efaa0c9ebe537a25c
rohan-aditeya <rohan.aditeya@gmail.com>
2025-10-28 22:01:37 +0530

    deleted unused dto class and updated spring version

--- src/main/java/com/elepha/solutions/rto/dto/VehicleListResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package com.elepha.solutions.rto.dto;
-
-import com.elepha.solutions.rto.model.VehicleInfo;
-
-import java.util.List;
-
-public record VehicleListResponseDTO(List<VehicleInfo> vehicles, long totalVehicles) {
-}
